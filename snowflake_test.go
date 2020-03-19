39638e6ba2307f293a8599ab5fc9dcbf577e21dc
blademainer <blademainer@gmail.com>
2020-03-19 23:19:42 +0800

    remove concurrent

--- snowflake_test.go
+++ snowflake_test.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"fmt"
 	"reflect"
-	"sync"
 	"testing"
 )
 
@@ -433,20 +432,9 @@ func BenchmarkGenerate(b *testing.B) {
 	b.ResetTimer()
 
 	fmt.Println("b.N: ", b.N)
-	sum := 0
-	wg := sync.WaitGroup{}
-	for i := 0; i < 100; i++ {
-		wg.Add(1)
-		go func() {
-			for n := 0; n < b.N; n++ {
-				_ = node.Generate()
-			}
-			sum += b.N
-			wg.Done()
-		}()
+	for n := 0; n < b.N; n++ {
+		_ = node.Generate()
 	}
-	wg.Wait()
-	fmt.Println("all: ", sum)
 }
 
 func BenchmarkGenerateMaxSequence(b *testing.B) {
